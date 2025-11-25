fee93b3b509b9fa00d2272b94405a92ddfe39c14
Ade Sofian <adesofianzz27@gmail.com>
2025-11-26 04:09:36 +0700

    Delete Tugas_1 directory

--- Tugas_1/assets/javascript/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
-
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    hamburger.classList.toggle('active');
-});
